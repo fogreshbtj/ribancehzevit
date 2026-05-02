9b034724f848d30a1e26ea2b7e95403aec6350ae
fogreshbtj <seanastore.btj@gmail.com>
2026-05-02 23:07:41 +0700

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -12,12 +12,19 @@ function booking() {
   const data = JSON.parse(localStorage.getItem("booking")) || [];
 
   const newData = {
-    id: "ZV-" + Date.now(),
-    nama: document.getElementById("nama").value,
-    nik: document.getElementById("nik").value,
-    wbp: document.getElementById("wbp").value,
-    status: "BOOKED"
-  };
+  id: "ZV-" + Date.now(),
+  nik: document.getElementById("nik").value,
+  nama: document.getElementById("nama").value,
+  gender: document.getElementById("gender").value,
+  relasi: document.getElementById("relasi").value,
+  pengikut: {
+    laki_laki: document.getElementById("ikut_l").value,
+    perempuan: document.getElementById("ikut_p").value,
+    anak: document.getElementById("ikut_a").value
+  },
+  wbp: document.getElementById("wbp").value,
+  status: "BOOKED"
+};
 
   data.push(newData);
   localStorage.setItem("booking", JSON.stringify(data));
