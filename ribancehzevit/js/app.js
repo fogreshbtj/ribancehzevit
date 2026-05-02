c6d36c02599b85429ba2b00156b5f1420decec16
fogreshbtj <seanastore.btj@gmail.com>
2026-05-02 13:03:56 +0700

    Delete ribancehzevit directory

--- ribancehzevit/js/app.js
+++
@@ -1,5 +0,0 @@
-const data = JSON.parse(localStorage.getItem("booking")) || [];
-
-document.getElementById("total").textContent = data.length;
-document.getElementById("hadir").textContent = data.filter(x => x.status === "HADIR").length;
-document.getElementById("noshow").textContent = data.filter(x => x.status === "BOOKED").length;
--- ribancehzevit/js/queue.js
+++
@@ -1,4 +0,0 @@
-const data = JSON.parse(localStorage.getItem("booking")) || [];
-const hadir = data.filter(x => x.status === "HADIR");
-
-document.getElementById("now").textContent = hadir.length ? hadir[0].id : "-";
