f7b74d9207ec665218e0b2e3b25bedb4977f39d0
fogreshbtj <seanastore.btj@gmail.com>
2026-05-03 00:10:03 +0700

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -8,6 +8,26 @@ if (wbpList && typeof daftarWBP !== "undefined") {
   });
 }
 
+function cekForm() {
+  const nik = document.getElementById("nik").value.trim();
+  const nama = document.getElementById("nama").value.trim();
+  const gender = document.getElementById("gender").value.trim();
+  const relasi = document.getElementById("relasi").value.trim();
+  const wbp = document.getElementById("wbp").value.trim();
+
+  const btn = document.getElementById("btn-daftar");
+
+  btn.disabled = !(nik && nama && gender && relasi && wbp);
+}
+
+["nik", "nama", "gender", "relasi", "wbp"].forEach(id => {
+  const el = document.getElementById(id);
+  if (el) {
+    el.addEventListener("input", cekForm);
+    el.addEventListener("change", cekForm);
+  }
+});
+
 function booking() {
   const btn = document.getElementById("btn-daftar");
 
