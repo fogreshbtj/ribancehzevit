db9a6fb58f1d9d99e264c9028c2b5a10716a00d7
fogreshbtj <seanastore.btj@gmail.com>
2026-05-02 12:55:17 +0700

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-let data = JSON.parse(localStorage.getItem("booking")) || [];
-
-document.getElementById("total").innerText = data.length;
-document.getElementById("hadir").innerText = data.filter(x => x.status === "HADIR").length;
-document.getElementById("noshow").innerText = data.filter(x => x.status === "BOOKED").length;
--- js/queue.js
+++
@@ -1,5 +0,0 @@
-let data = JSON.parse(localStorage.getItem("booking")) || [];
-
-let hadir = data.filter(x => x.status === "HADIR");
-
-document.getElementById("now").innerText = hadir.length ? hadir[0].id : "-";
